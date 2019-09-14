19b06d56c47105cf734cc9dc23270c724ec5f9b9
phamtobao <phamtobao99@gmail.com>
2019-09-14 20:19:43 +0700

    14.9

---
+++ demoexpress/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => res.send("<h1>Hello World! Im bao</h1>"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
